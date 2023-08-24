69a1de4b6e0d90b3a386e655d0c739a0d43f515f
Danilo Antunes Leal <96276849+daniloantunesleal@users.noreply.github.com>
2023-08-23 23:22:38 -0300

    Correction of Headings h1, h2, h3 ...

--- src/components/GridContent/index.jsx
+++ src/components/GridContent/index.jsx
@@ -8,7 +8,7 @@ export const GridContent = ({ title, html, background = false }) => {
     return (
         <SectionBackground background={background}>
             <Styled.Container>
-                <Heading uppercase colordark={!background}>
+                <Heading uppercase colordark={!background} as="h2">
                     {title}
                 </Heading>
                 <Styled.Html>
--- src/components/GridTwoColumn/index.jsx
+++ src/components/GridTwoColumn/index.jsx
@@ -9,7 +9,7 @@ export const GridTwoColumn = ({ title, text, srcImg, background = false }) => {
         <SectionBackground background={background}>
             <Styled.Container background={background}>
                 <Styled.TextContainer>
-                    <Heading uppercase colordark={!background}>
+                    <Heading uppercase colordark={!background} as="h2">
                         {title}
                     </Heading>
                     <TextComponent>{text}</TextComponent>
