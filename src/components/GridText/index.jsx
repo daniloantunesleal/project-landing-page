c9c6b461b20d41e7ea41f88ae425328031f45b11
Danilo Antunes Leal <96276849+daniloantunesleal@users.noreply.github.com>
2023-08-23 23:19:13 -0300

    GridText

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,64 @@
+import P from "prop-types";
+import * as Styled from "./styles";
+import { SectionBackground } from "../SectionBackground";
+import { Heading } from "../Heading";
+import { TextComponent } from "../TextComponent";
+
+export const GridText = ({ title, description, grid, background = false }) => {
+    return (
+        <SectionBackground background={background}>
+            <Styled.Container>
+                <Heading size="huge" uppercase colordark={!background}>
+                    {title}
+                </Heading>
+                <TextComponent>{description}</TextComponent>
+                <Styled.Grid>
+                    {grid.map((el) => (
+                        <Styled.GridElement key={el.title}>
+                            <Heading size="medium" colordark={!background}>
+                                {el.title}
+                            </Heading>
+                            <TextComponent>{el.description}</TextComponent>
+                        </Styled.GridElement>
+                    ))}
+                </Styled.Grid>
+            </Styled.Container>
+        </SectionBackground>
+    );
+};
+
+GridText.propTypes = {
+    title: P.string.isRequired,
+    description: P.string.isRequired,
+    grid: P.arrayOf(
+        P.shape({
+            title: P.string.isRequired,
+            description: P.string.isRequired,
+        }),
+    ).isRequired,
+    background: P.bool,
+};
+
+/*
+
+import P from "prop-types";
+import * as Styled from "./styles";
+import { SectionBackground } from "../SectionBackground";
+import { Heading } from "@storybook/blocks";
+
+export const GridSection = ({ title }) => {
+    return (
+        <SectionBackground>
+            <Styled.Container>
+                <Heading>{title}</Heading>
+            </Styled.Container>
+        </SectionBackground>
+    );
+};
+
+GridSection.propTypes = {
+    title: P.string.isRequired,
+};
+
+
+*/
